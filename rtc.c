259bbaaaf323fa021c030361049e54534f4e23c4
Jean-Christophe Rona <jc@rona.fr>
2017-03-13 14:36:21 +0100

    Fix warnings related to pointer-sign

--- rtc.c
+++ rtc.c
@@ -106,58 +106,58 @@ uint32_t rtc_get_current_time(void) {
 }
 
 uint32_t rtc_get_current_time_in_seconds(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_hour*3600 + time_struct->tm_min*60 + time_struct->tm_sec;
 }
 
 uint32_t rtc_get_current_hour_24(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_hour;
 }
 
 uint32_t rtc_get_current_hour_12(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  uint32_t h = time_struct->tm_hour % 12;
 		return h == 0 ? 12 : h;
 }
 
 uint32_t rtc_get_current_hour_12_designator(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_hour / 12;
 }
 
 uint32_t rtc_get_current_minutes(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_min;
 }
 
 uint32_t rtc_get_current_seconds(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_sec;
 }
 
 uint32_t rtc_get_current_day_of_week(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_wday + 1;
 }
 
 uint32_t rtc_get_current_day_of_month(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_mday;
 }
 
 uint32_t rtc_get_current_day_of_year(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_yday + 1;
 }
 
 uint32_t rtc_get_current_month(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_mon + 1;
 }
 
 uint32_t rtc_get_current_year(void) {
-    struct tm* time_struct = localtime(&current_time);
+    struct tm* time_struct = localtime((const time_t*) &current_time);
 	  return time_struct->tm_year + 1900;
 }
 
